100f201d0c19fda0ef319c55e65bd6d65dbd135e
Sivakaran <Sivakaran@DESKTOP-AUHSFF3>
2025-11-10 09:52:04 +0000

    Test CI pipeline

---
+++ DevOpsDemo/WeatherForecastControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using DevOpsDemo.Controllers;
+using Moq;
+using Xunit;
+
+namespace DevOpsDemo
+{
+    public class WeatherForecastControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsWeatherForecast()
+        {
+            // 1. Mock the logger
+            var mockLogger = new Mock<ILogger<WeatherForecastController>>();
+
+            // 2. Pass the mock to the controller
+            var controller = new WeatherForecastController(mockLogger.Object);
+
+            // 3. Call the action
+            var result = controller.Get();
+
+            // 4. Assert the result
+            Assert.NotEmpty(result);
+        }
+    }
+}
